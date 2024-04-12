610c58501a587877fdfe09e87c84be6691fa4362
Karol Atys <karol.atys@gmail.com>
2024-04-12 14:15:37 +0200

    feat: tags page now actually does something

--- lib/Pages/tags_page.dart
+++ lib/Pages/tags_page.dart
@@ -14,8 +14,72 @@ class TagsPage extends StatefulWidget {
 }
 
 class _TagsPageState extends State<TagsPage> {
+
+  TextEditingController tagController = TextEditingController();
+  TextEditingController relatedHashtagController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tags'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: const InputDecoration(
+                        hintText: 'Add a tag',
+                        prefix: Text('@'),
+                      ),
+                      controller: tagController,
+                    ),
+                    TextField(
+                      decoration: const InputDecoration(
+                        hintText: 'Add a related hashtag',
+                        prefix: Text('#'),
+                      ),
+                      controller: relatedHashtagController,
+                    ),
+                    
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 50,
+                child: ElevatedButton(
+                      onPressed: () {
+                        widget.dataHandler.tagsList.add(Tag(
+                          tag: tagController.text,
+                          hashtag: relatedHashtagController.text,
+                        ));
+                        tagController.clear();
+                        relatedHashtagController.clear();
+                        widget.dataHandler.saveTagsList(widget.dataHandler.tagsList);
+                        setState(() {
+                          
+                        });
+                      },
+                      child: const Text('Add'),
+                    ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.dataHandler.tagsList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('@${widget.dataHandler.tagsList[index].tag}'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
