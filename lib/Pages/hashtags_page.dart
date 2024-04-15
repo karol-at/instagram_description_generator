be52aadd8959b85326585171d21edc75b102e931
Karol Atys <karol.atys@gmail.com>
2024-04-15 10:47:48 +0200

    laid out page skeleton

--- lib/Pages/hashtags_page.dart
+++ lib/Pages/hashtags_page.dart
@@ -1,6 +1,7 @@
 library hashtags_page;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:instagram_description_generator/Utils/appstate.dart';
 import 'package:provider/provider.dart';
 
@@ -12,10 +13,63 @@ class HashtagsPage extends StatefulWidget {
 }
 
 class _HashtagsPageState extends State<HashtagsPage> {
+
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController hashtagController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Consumer<MyAppState>(
-      builder: (context, data, child) => const Placeholder()
-      );
+      builder: (context, data, child) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Hashtags'),
+        ),
+        body: Column(
+          children: [
+            
+            Expanded(
+              child: ListView.builder(
+                itemCount: data.dataHandler.categoryList.length,
+                itemBuilder: (context, index) {
+                  return const Placeholder(
+                    fallbackHeight: 100,
+                    fallbackWidth: 100,)
+                  ;
+                },
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(context: context, builder: (context) => SimpleDialog(
+              title: const Text('Add a new hashtag'),
+              contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 5),
+              children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Category name',
+                      prefix: SizedBox(width: 15),
+                    ),
+                    controller: categoryController,
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Hashtag',
+                      prefix: SizedBox(width: 15, child: Text('#')),
+                    ),
+                    controller: hashtagController,
+                  ),
+                  TextButton(onPressed: (){},
+                   child: const Text('Add')
+                   )
+                ],
+              )
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
