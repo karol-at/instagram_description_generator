494114e2247167e655694fa1ebc22d667138b600
Karol Atys <karol.atys@gmail.com>
2024-04-15 16:37:49 +0200

    moved tag adding to a popup window triggered with a button

--- lib/Pages/tags_page.dart
+++ lib/Pages/tags_page.dart
@@ -20,53 +20,20 @@ class _TagsPageState extends State<TagsPage> {
   @override
   Widget build(BuildContext context) {
     return Consumer<MyAppState>(
-      builder: (context, data, child) => Scaffold(
+      builder: (context, state, child) => Scaffold(
         appBar: AppBar(
           title: const Text('Tags'),
         ),
         body: Column( 
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    children: [
-                      TextField(
-                        decoration: const InputDecoration(
-                          hintText: 'Add a tag',
-                          prefix: SizedBox(width: 15, child: Text('@')),
-                        ),
-                        controller: tagController,
-                        onEditingComplete: (){data.dataHandler.addTag(tagController, relatedHashtagController); setState(() {});},
-                      ),
-                      TextField(
-                        decoration: const InputDecoration(
-                          hintText: 'Add a related hashtag',
-                          prefix: SizedBox(width: 15, child: Text('#')),
-                        ),
-                        controller: relatedHashtagController,
-                        onEditingComplete: (){data.dataHandler.addTag(tagController, relatedHashtagController); setState(() {});}
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: 50,
-                  child: IconButton(
-                    onPressed: (){data.dataHandler.addTag(tagController, relatedHashtagController); setState(() {});},
-                    icon: const Icon(IconicIcons.plus),
-                  ),
-                ),
-              ],
-            ),
             Expanded(
               child: ListView.builder(
-                itemCount: data.dataHandler.tagsList.length,
+                itemCount: state.dataHandler.tagsList.length,
                 itemBuilder: (context, index) {
                   return ListTile(
                     title: Row(
                       children: [
-                        Expanded(child: Text('@${data.dataHandler.tagsList[index].tag}')),
+                        Expanded(child: Text('@${state.dataHandler.tagsList[index].tag}')),
                         Container(
                           decoration: const BoxDecoration(
                             borderRadius: BorderRadius.only(
@@ -79,16 +46,16 @@ class _TagsPageState extends State<TagsPage> {
                               IconButton(
                                 icon: const Icon(IconicIcons.trash),
                                 onPressed: (){
-                                  data.dataHandler.tagsList.removeAt(index);
-                                  data.dataHandler.saveTagsList();
+                                  state.dataHandler.tagsList.removeAt(index);
+                                  state.dataHandler.saveTagsList();
                                   setState(() {                          
                                   });
                                 },
                               ),
-                              Checkbox(value: data.dataHandler.tagsList[index].selected,
+                              Checkbox(value: state.dataHandler.tagsList[index].selected,
                               onChanged: (bool? value) {                             
                                 setState(() {
-                                  data.dataHandler.tagsList[index].selected = value!;
+                                  state.dataHandler.tagsList[index].selected = value!;
                                 });
                                 }
                               )
@@ -102,6 +69,41 @@ class _TagsPageState extends State<TagsPage> {
               ),
             ),
           ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(context: context, builder: (context) => SimpleDialog(
+              title: const Text('Add a new hashtag'),
+              contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 5),
+              children: [
+                      TextField(
+                        decoration: const InputDecoration(
+                          hintText: 'Add a tag',
+                          prefix: SizedBox(width: 15, child: Text('@')),
+                        ),
+                        controller: tagController,
+                      ),
+                      TextField(
+                        decoration: const InputDecoration(
+                          hintText: 'Add a related hashtag',
+                          prefix: SizedBox(width: 15, child: Text('#')),
+                        ),
+                        controller: relatedHashtagController,
+                      ),
+                  TextButton(onPressed: (){
+                    
+                    Navigator.of(context).pop();
+                    setState(() {
+                      state.dataHandler.addTag(tagController, relatedHashtagController);
+                    });
+                  },
+                   child: const Text('Add')
+                  )
+                ],
+              )
+            );
+          },
+          child: const Icon(Icons.add),
         ),
       ),
     );
