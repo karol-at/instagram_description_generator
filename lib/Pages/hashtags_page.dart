cc903adc8cdeb96dddf9b7e5fab53229d6d7c789
Karol Atys <karol.atys@gmail.com>
2024-05-05 12:02:22 +0200

    decouple hashtag adding from category adding

--- lib/Pages/hashtags_page.dart
+++ lib/Pages/hashtags_page.dart
@@ -32,23 +32,67 @@ class _HashtagsPageState extends State<HashtagsPage> {
               child: ListView.builder(
                 itemCount: state.dataHandler.categoryList.length,
                 itemBuilder: (context, index) {
-                  return Card(
-                    margin: const EdgeInsets.all(10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.all(10),
-                          child: Text(state.dataHandler.categoryList[index], style: Theme.of(context).textTheme.titleMedium,)
-                          ),
-                        Wrap(
+                  return Stack(
+                    children: [
+                      Card(
+                        margin: const EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            for (String hashtag in state.dataHandler.hashtagList[index])
-                              HashtagBox(hashtag: hashtag, index: index),
+                            Container(
+                              margin: const EdgeInsets.all(10),
+                              child: Text(state.dataHandler.categoryList[index], style: Theme.of(context).textTheme.titleMedium,)
+                            ),
+                            Wrap(
+                              children: [
+                                for (String hashtag in state.dataHandler.hashtagList[index])
+                                  HashtagBox(hashtag: hashtag, index: index),
+                              ],
+                            ),
                           ],
+                        )
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Padding(
+                          padding: const EdgeInsets.all(14),
+                          child: IconButton(
+                            icon: const Icon(Icons.add),
+                            onPressed: () {
+                              showDialog(context: context, builder: (context) => SimpleDialog(
+                                title: const Text('Add a new hashtag'),
+                                contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 5),
+                                children: [
+                                  TextField(
+                                    decoration: const InputDecoration(
+                                      hintText: 'Hashtag',
+                                      prefix: SizedBox(width: 15, child: Text('#')),
+                                    ),
+                                    controller: hashtagController,
+                                  ),
+                                  TextButton(
+                                    onPressed: (){
+                                      if (hashtagController.text.isEmpty) {
+                                        return;
+                                      }
+                                      if (!state.dataHandler.hashtagList[index].contains(hashtagController.text)) {
+                                        state.dataHandler.hashtagList[index].add(hashtagController.text);
+                                        state.dataHandler.saveList(state.dataHandler.hashtagList[index], state.dataHandler.categoryList[index]);
+                                      }
+                                      Navigator.of(context).pop();
+                                      setState(() {
+                                      
+                                      });
+                                    }, 
+                                    child: const Text('Add')
+                                  ),
+                                ],
+                              ));
+                            },
+                          ),
                         ),
-                      ],
-                    )
+                      ),
+                    ],
                   );
                 },
               ),
@@ -57,68 +101,35 @@ class _HashtagsPageState extends State<HashtagsPage> {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            //TODO: decouple category adding from hashtag adding
             showDialog(context: context, builder: (context) => SimpleDialog(
               title: const Text('Add a new hashtag'),
               contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 5),
               children: [
-                  DropdownButton<String>(
-                    hint: const Text('Select a category'),
-                    items: state.dataHandler.categoryList.map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList() + const [
-                      DropdownMenuItem<String>(
-                        value: 'New category',
-                        child: Text('New category'),
-                      ),
-                    ],
-                    onChanged: (String? value) {
-                      addNewCategory = value == 'New category' ? true : false;
-                      setState(() {});
-                    },
+                TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'New category',
+                    prefix: SizedBox(width: 15),
                   ),
-                  //TODO: figure out why this doesn't work
-                  addNewCategory ? TextField(
-                    decoration: const InputDecoration(
-                      hintText: 'New category',
-                      prefix: SizedBox(width: 15),
-                    ),
-                    controller: categoryController,
-                  ) : const SizedBox(),
-                  TextField(
-                    decoration: const InputDecoration(
-                      hintText: 'Hashtag',
-                      prefix: SizedBox(width: 15, child: Text('#')),
-                    ),
-                    controller: hashtagController,
-                  ),
-                  TextButton(onPressed: (){
-                    if (categoryController.text.isEmpty || hashtagController.text.isEmpty) {
-                      return;
-                    }
-                    if (!state.dataHandler.categoryList.contains(categoryController.text)) {
-                      state.dataHandler.categoryList.add(categoryController.text);
-                      state.dataHandler.saveList(state.dataHandler.categoryList, 'categoryList');
-                      state.dataHandler.hashtagList.add([]);
-                    }
-                    int index = state.dataHandler.categoryList.indexOf(categoryController.text);
-                    if (!state.dataHandler.hashtagList[index].contains(hashtagController.text)) {
-                      state.dataHandler.hashtagList[index].add(hashtagController.text);
-                    }
-                    state.dataHandler.saveList(state.dataHandler.hashtagList[index], state.dataHandler.categoryList[index]);
-                    Navigator.of(context).pop();
-                    setState(() {
-                      
-                    });
-                  },
-                   child: const Text('Add')
-                  )
+                  controller: categoryController,
+                ), 
+                TextButton(onPressed: (){
+                  if (categoryController.text.isEmpty) {
+                    return;
+                  }
+                  if (!state.dataHandler.categoryList.contains(categoryController.text)) {
+                    state.dataHandler.categoryList.add(categoryController.text);
+                    state.dataHandler.saveList(state.dataHandler.categoryList, 'categoryList');
+                    state.dataHandler.hashtagList.add([]);
+                  }
+                  Navigator.of(context).pop();
+                  setState(() {
+                    
+                  });
+                },
+                  child: const Text('Add')
+                )
                 ],
-              )
-            );
+              ));
           },
           child: const Icon(Icons.add),
         ),
